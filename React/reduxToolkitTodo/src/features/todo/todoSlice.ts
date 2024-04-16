11a9403196196d4d510da9c252d923c4f2517347
Yug Jadvani <110123287+yugjadvani-dev@users.noreply.github.com>
2024-04-16 18:31:35 +0530

    Redux toolkit todoapp add & remove

---
+++ React/reduxToolkitTodo/src/features/todo/todoSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice, nanoid, PayloadAction } from "@reduxjs/toolkit";
+
+interface Todo {
+  id: string;
+  text: string;
+}
+
+interface TodoState {
+  todos: Todo[];
+}
+
+const initialState: TodoState = {
+  todos: [],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<string>) => {
+      const todo: Todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action: PayloadAction<string>) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
