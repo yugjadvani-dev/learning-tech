2706d6b63371ac389d3665399023f4d65b9a1721
Yug Jadvani <yugjadvani.dev@gmail.com>
2024-05-18 11:29:55 +0530

    Add mongoose

---
+++ NextJs/zod-validation/src/model/User.ts
@@ -0,0 +1,8 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({});
