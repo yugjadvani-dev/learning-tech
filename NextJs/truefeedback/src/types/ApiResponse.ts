d332b4de20f146a9acc4270a69d170f23059d0d9
yug jadvani <yugjadvani.dev@gmail.com>
2024-07-06 22:00:43 +0530

    remmove dev

--- NextJs/truefeedback/src/types/ApiResponse.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from "@/model/User";
-
-export interface ApiResponse {
-  success: boolean;
-  message: string;
-  isAcceptingMessage?: boolean;
-  messages?: Array<Message>;
-}
--- currencyConverter/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable prettier/prettier */
-interface Currency {
-  id: number;
-  name: string;
-  value: number;
-  flag: string;
-  symbol: string;
-}
