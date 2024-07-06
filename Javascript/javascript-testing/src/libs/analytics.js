a093c6999276f0d56a577954ed03147247d79b70
yug jadvani <yugjadvani.dev@gmail.com>
2024-07-06 21:44:17 +0530

    remove all from nextjs

--- Javascript/javascript-testing/src/libs/analytics.js
+++
@@ -1,7 +0,0 @@
-import delay from 'delay';
-
-export async function trackPageView(pagePath) {
-  console.log(`Sending analytics...`);
-  console.log(`Path: ${pagePath}`);
-  await delay(3000);
-}
--- Python/Python-Learn/02_conditionals/07_solution.py
+++
@@ -1,9 +0,0 @@
-order_size = "Small"
-extra_shot = True
-
-if extra_shot:
-    coffee = order_size + " coffee with an extra shot"
-else:
-    coffee = order_size + " coffee"
-
-print("order: " + coffee)
