95cdc01a8be617b11fd91e5402d370420b11963b
yug jadvani <yugjadvani.dev@gmail.com>
2024-07-06 21:49:24 +0530

    remove all from react-native

--- Javascript/javascript-testing/src/libs/security.js
+++
@@ -1,7 +0,0 @@
-export function generateCode() {
-  return Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
-}
-
-export default {
-  generateCode,
-};
--- Python/Python-Learn/03_loops/04_solution.py
+++
@@ -1,7 +0,0 @@
-str = "Python"
-reversed_str = ""
-
-for char in str:
-    reversed_str = char + reversed_str
-
-print(reversed_str)
