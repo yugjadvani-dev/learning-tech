a76bd8ebf840442d9db5394f05e96fc437a8e698
yug jadvani <yugjadvani.dev@gmail.com>
2024-07-06 22:03:40 +0530

    main

--- NextJs/search-filter/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"NKohKb1mLdeFrbKJysiqFmhbspXz+hV6yth/wvW/GMc=\"\n}"
--- Python/Python-Learn/05_functions/09_solution.py
+++
@@ -1,9 +0,0 @@
-def even_generator(limit):
-    # li = []
-    for i in range(2, limit + 1, 2):
-        yield i
-        # li.append(i)
-    # return li
-
-for num in even_generator(10):
-    print(num)
