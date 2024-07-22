9bc6474773e5ddfe70357c7b210a0238fc7aa287
yug jadvani <yugjadvani.dev@gmail.com>
2024-07-22 22:54:21 +0530

    day1 and day2 done

---
+++ 30-days-javascript-challenge/day1.js
@@ -0,0 +1,34 @@
+// Variable and Data Types
+
+// Variable declarations
+var num = 40
+console.log(num)
+console.log(typeof num)
+
+let str = "Hello, world!"
+console.log(str)
+console.log(typeof str)
+
+const bool = true
+console.log(bool)
+console.log(typeof bool)
+
+const arr = ["hello", "world"]
+console.log(arr)
+console.log(typeof arr)
+
+const obj = {name: "world"}
+console.log(obj)
+console.log(typeof obj)
+
+// Reassign the value
+let val = "Hii there"
+console.log(val)
+val = 1000
+console.log(val)
+
+// understading const
+const constValue = "I am constant"
+console.log(constValue)
+// constValue = 1000
+// console.log(constValue)
---
+++ 30-days-javascript-challenge/day2.js
@@ -0,0 +1,53 @@
+// Operators
+
+// Arithmetic Operators
+let sum = 10 + 20
+console.log(sum)
+
+let subtraction = 20 - 10
+console.log(subtraction)
+
+let multiplication = 10 * 20
+console.log(multiplication)
+
+let division = 20 / 10
+console.log(division)
+
+let reminder = 13 % 5
+console.log(reminder)
+
+// Assignment Operators
+let x = 10
+x += 5
+console.log(x)
+
+let y = 15
+y -= 5
+console.log(y)
+
+// Comparison Operators
+let a = 10
+let b = 20
+console.log(a > b) // false
+console.log(a < b) // true
+console.log(a >= b) // false
+console.log(a <= b) // true
+console.log(a == b) // false
+console.log(a === b) // false
+console.log(a !== b) // true
+
+let c = "30"
+let d = 30
+console.log(c == d) // true
+console.log(c === d) // false
+
+// Logical Operators
+let e = true
+let f = false
+console.log(e && f) // false
+console.log(e || f) // true
+console.log(!e) // false
+
+// Terminal Operators
+let positiveNum = 1
+console.log(positiveNum >= 0 ? "Is Positive" : "Is Negative") // true
