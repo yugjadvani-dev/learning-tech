e3d383723a3c74cf589e53feea709f9ea991ecc4
Yug Jadvani - Delemont <yug@delemontstudio.com>
2024-07-23 19:45:45 +0530

    add day5

---
+++ 30-days-javascript-challenge/day5.js
@@ -0,0 +1,17 @@
+// Functions
+
+// Function declaration
+function oddeven(num){
+if (num % 2 == 0){
+    console.log(num + ' is Even')
+}else {
+    console.log(num + ' is Odd')
+}
+}
+oddeven(4)
+
+// Square of numbers
+function square(num){
+    return num * num
+}
+console.log(square(5))
