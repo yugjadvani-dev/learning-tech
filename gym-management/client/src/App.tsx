4bf35b92e245181752aa01239bedc1c40eff2e86
Yug Jadvani <yugjadvani.dev@gmail.com>
2024-08-17 22:58:52 +0530

    setup for gym and healthcare

---
+++ gym-management/client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Routes>
+      {/* <Route path='/' element={} /> */}
+    </Routes>
+  );
+};
+
+export default App;
---
+++ healthcare/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <>
+    <Routes>
+      {/* <Route path='/' element={} /> */}
+    </Routes>
+    </>
+  )
+}
+
+export default App
