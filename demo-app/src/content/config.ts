1915970e7007e979259122dc8525ef3ac859d7bf
yug jadvani <yugjadvani.dev@gmail.com>
2024-08-26 10:21:56 +0530

    add paage in astro

--- demo-app/src/content/config.ts
+++ demo-app/src/content/config.ts
@@ -1,14 +1,15 @@
 import { defineCollection, z } from "astro:content";
 
 const postsCollection = defineCollection({
-    schema: z.object({
-        author: z.string(),
-        date: z.string(),
-        image: z.string(),
-        title: z.string(),
-    })
+  schema: ({ image }) =>
+    z.object({
+      author: z.string(),
+      date: z.string(),
+      image: image(),
+      title: z.string(),
+    }),
 });
 
 export const collections = {
-    posts: postsCollection,
+  posts: postsCollection,
 };
