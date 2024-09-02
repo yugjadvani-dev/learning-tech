29d842b333ce9a18e299640a1b5097e4578a47f9
yug jadvani <110123287+yugjadvani-dev@users.noreply.github.com>
2024-09-02 22:28:19 +0530

    init app

--- movie-review/client/src/App.tsx
+++ movie-review/client/src/App.tsx
@@ -1,44 +1,9 @@
-import { Route, Routes, useLocation } from "react-router-dom";
-import { AboutUs } from "./pages/about-us";
-import { AccountSetting } from "./pages/account-setting";
-import { ContactUs } from "./pages/contact-us";
-import { Dashboard } from "./pages/dashboard";
-import { FAQ } from "./pages/faq";
-import { Home } from "./pages/home";
-import { Jobs } from "./pages/jobs";
-import { Login } from "./pages/login";
-import { PressRoom } from "./pages/press-room";
-import { PrivacyPolicy } from "./pages/privacy-policy";
-import { Register } from "./pages/register";
-import { Services } from "./pages/services";
-import { TermsofServices } from "./pages/terms-of-services";
-import { UserRating } from "./pages/user-rating";
-import { Wishlist } from "./pages/wishlist";
+import React from 'react'
 
 const App = () => {
-  const { pathname } = useLocation();
-
   return (
-    <>
-      <Routes>
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/account-setting" element={<AccountSetting />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/jobs" element={<Jobs />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/press-room" element={<PressRoom />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/terms-of-services" element={<TermsofServices />} />
-        <Route path="/user-rating" element={<UserRating />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-      </Routes>
-    </>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
