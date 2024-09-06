caebb8335803bc179b0308f35dae47e4205e5d0d
yug jadvani <110123287+yugjadvani-dev@users.noreply.github.com>
2024-09-06 23:45:15 +0530

    operators

---
+++ operators.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a = 10, b = 5;
+    cout << "sum = " << (a + b) << endl;
+    cout << "difference = " << (a - b) << endl;
+    cout << "product = " << (a * b) << endl;
+    cout << "division = " << (a / b) << endl;
+    cout << "modulo = " << (a % b) << endl;
+    return 0;
+}
